6d8e9ee7754f9a0a692dd1b26fd9f293c6a4420c
Tymeckia Kendall <tykend@gmail.com>
2018-11-20 01:07:22 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -18,6 +18,8 @@ end
     elsif phrase == phrase.upcase
       return "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
-
+else
+  		return "NO, NOT SINCE 1938!"
+    end
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
